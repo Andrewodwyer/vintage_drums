d4d17bba9d36e94acca0e8c548591b5593e4e42b
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-12-03 10:37:52 +0000

    add dropdown to main nav

--- products/urls.py
+++ products/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 from .views import product_detail
 
-# urlpatterns = [
-#     path('', views.all_products, name='products')
-# ]
-
 urlpatterns = [
     path('', views.all_products, name='all_products'),
     path('<int:product_id>/', product_detail, name='product_detail'),  # Function-based view int as it is a number
