9d9aa2f7ea4f9d2c0f36af6209ea2a17936d3247
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-12-17 20:19:45 +0000

    Added products, home page now showing first 4 drums and cymbals

--- home/views.py
+++ home/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 from products.models import Product, Category
 
 def index(request):
-    # Fetch all categories to display in the categories section
     categories = Category.objects.all()
 
-    # Get the top 4 highest-rated drum kits (category: 'Drum Kit')
-    featured_drum_kits = Product.objects.filter(category__friendly_name='Drum Kit').order_by('-rating')[:4]
+    # highest-rated drum kits (category: 'Drum Kit')
+    featured_drum_kits = Product.objects.filter(category__friendly_name='Drum Kits').order_by('-rating')[:4]
 
-    # Get the top 4 highest-rated cymbals (category: 'Cymbal')
-    featured_cymbals = Product.objects.filter(category__friendly_name='Cymbal').order_by('-rating')[:4]
+    # highest-rated cymbals (categories: 'hi_hats', 'crash_cymbals', 'ride_cymbals')
+    cymbal_categories = ['hi_hats', 'ride_cymbal', 'crash_cymbal']
+    featured_cymbals = Product.objects.filter(category__name__in=cymbal_categories).order_by('-rating')[:4]
 
-    # Pass the categories and featured drum kits and cymbals to the template
     return render(request, 'home/index.html', {
         'categories': categories,
         'featured_drum_kits': featured_drum_kits,
